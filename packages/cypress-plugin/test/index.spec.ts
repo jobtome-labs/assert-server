54f420f2ab2bceb0c3a270b714bc23af7345d5bb
Erik Olivero <erikpc@MacBook-Pro-di-Erik.local>
2022-12-15 07:49:01 +0100

    improve sample test suite

--- packages/cypress-plugin/test/index.spec.ts
+++ packages/cypress-plugin/test/index.spec.ts
@@ -8,4 +8,7 @@ describe("chai-simple-test", () => {
   it("should pass if is a valid foo", () => {
     expect("foo").to.be.foo();
   });
+  it("should [to.not.be] pass is is not a valid foo", () => {
+    expect("bar").to.not.be.foo();
+  });
 });
